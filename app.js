e9d2c4386acbba27705350d87a247e632b906b37
Pancho <franciscopc3@outlook.com>
2020-07-23 16:31:14 -0300

    third run

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var mercadopago = require('mercadopago') ;
+require('dotenv').config();
  
 var app = express();
  
@@ -10,12 +12,95 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/succes', function (req, res) {
+    res.render('succes');
+});
+
+app.get('/pending', function (req, res) {
+    res.render('pending');
+});
+
+app.get('/failure', function (req, res) {
+    res.render('failure');
+});
+
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
+
+app.post('/checkout', async function (req,res)  {
+
+    console.log(req.query);
+    
+        let preference = {
+            items: [
+                {   id: 1234,
+                    title: req.query.titulo,
+                    description: "Esta es una descripcion",
+                    unit_price: parseInt(req.query.unit_price),
+                    quantity: 1,
+                    picture_url: "https://samsungar.vtexassets.com/arquivos/ids/162059-800-auto?width=800&height=auto&aspect=true",
+                    external_reference: "franciscopc3@gmail.com"
+
+
+                    
+                }
+            ],
+            payer: {
+                name: 'Lalo',
+                surname: 'Landa',
+                email: 'test_user_63274575@testuser.com',
+                phone: {
+                    area_code: "11",
+                    number: 22223333  
+                },
+                address: {
+                    street_name: 'False',
+                    street_number: 123,
+                    zip_code: "1111"
+                }
+            },
+            payment_methods: {
+                excluded_payment_methods: [
+                    {
+                        id: "amex"
+                    }
+                ],
+                excluded_payment_types: [
+                    {
+                        id: "atm"
+                    }
+                ],
+                installments: 6,
+            },
+
+            back_urls: {
+                success: "https://franpc3-mp-ecommerce-nodejs.herokuapp.com/succes?collection_id=[PAYMENT_ID]&collection_status=approved&external_reference=[EXTERNAL_REFERENCE]&payment_type=credit_card&preference_id=[PREFERENCE_ID]&site_id=[SITE_ID]&processing_mode=aggregator&merchant_account_id=nul",
+                pending: "https://franpc3-mp-ecommerce-nodejs.herokuapp.com/pending",
+                failure: "https://franpc3-mp-ecommerce-nodejs.herokuapp.com/failure"
+            },
+            auto_return: "approved",
+           
+        };
+    
+        mercadopago.preferences.create(preference)
+            .then(function (response) {
+                // Este valor reemplazará el string "$$init_point$$" en tu HTML
+                res.json({ response});
+            }).catch(function (err) {
+               console.log(err);
+            });
+     
+    
+} )
 
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
 app.listen(process.env.PORT || 3000);
+
+mercadopago.configure({
+    access_token: process.env.MP_TOKEN
+  });
+  
